96f602c2a14aa6147a770dda391b172ea1aee5d0
sazosalgadojorge <sazosalgadojorge@gmail.com>
2026-02-24 22:08:19 -0300

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { LanguageProvider } from './context/LanguageContext.jsx'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <LanguageProvider>
         <App />
+        <SpeedInsights />
       </LanguageProvider>
     </BrowserRouter>
   </StrictMode>,
